500834f086e280c387772e39f556d6f54b83f891
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-11-06 09:43:53 +0100

    Ejer3 Boletin 5

---
+++ Ejer3-boletin5/src/ejer3/boletin5/Ejer3Boletin5.java
@@ -0,0 +1,14 @@
+
+package ejer3.boletin5;
+
+import java.util.Scanner;
+
+public class Ejer3Boletin5 {
+
+    public static void main(String[] args) {
+        Metodo algo = new Metodo();
+        Scanner entrada = new Scanner (System.in);
+        algo.metodComprobacion(entrada.nextInt());
+    }
+    
+}
---
+++ Ejer3-boletin5/src/ejer3/boletin5/Metodo.java
@@ -0,0 +1,22 @@
+
+package ejer3.boletin5;
+
+public class Metodo {
+    
+    public Metodo(){
+        
+    }
+    
+    public void metodComprobacion(int num){
+        
+        if(num>0){
+            System.out.println("+");
+        }else if(num<0){
+            System.out.println("-");
+        }else{
+            System.out.println("0");
+        }
+    }
+    
+    
+}
