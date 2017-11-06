9b61c22d7f3eb09e3576bbeb64ff16fe7d063208
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-11-06 09:39:24 +0100

    Ejer1 Boletin 5

---
+++ Ejer1-boletin5/src/ejer1/boletin5/Ejer1Boletin5.java
@@ -0,0 +1,17 @@
+
+package ejer1.boletin5;
+
+import java.util.Scanner;
+
+public class Ejer1Boletin5 {
+
+    public static void main(String[] args) {
+        Metodo algo = new Metodo();
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("mete número");
+        algo.numPositivo(entrada.nextInt());
+   
+        
+    }
+    
+}
---
+++ Ejer1-boletin5/src/ejer1/boletin5/Metodo.java
@@ -0,0 +1,15 @@
+
+package ejer1.boletin5;
+
+public class Metodo {
+    
+    public Metodo(){
+        
+    }
+    
+    public  void numPositivo(int num){
+             if(num>=0)
+            System.out.println("número positivo");
+        
+    }
+}
